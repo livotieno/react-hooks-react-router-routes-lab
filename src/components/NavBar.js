01c64c7f2a97cb748e636c443e33433e3a4a5b61
livotieno <livio5261@gmail.com>
2023-11-03 10:33:16 +0300

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,38 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  const navStyles = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "bold" : "normal",
+    };
+  };
+  
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <NavLink style={navStyles} to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink style={navStyles} to="/movies">
+            Movies
+          </NavLink>
+        </li>
+        <li>
+          <NavLink style={navStyles} to="/directors">
+            Directors
+          </NavLink>
+        </li>
+        <li>
+          <NavLink style={navStyles} to="/actors">
+            Actors
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default NavBar;
